e59fd4bb3de7a320f9fc7c1dafc50771f0c299ed
valeng1133 <est.yulianav.gomez@unimilitar.edu.co>
2024-08-27 15:25:55 -0500

    Create pg1.java

---
+++ P1/pg1.java
@@ -0,0 +1,10 @@
+Public class Triangulo {
+    private int base ;
+    private int altura; 
+}
+public Triangulo(){
+public main (String [] args);
+calcTriangul = Triangulo; 
+system.println.out "Triangulo es=  "+base * + altura
+
+}   
