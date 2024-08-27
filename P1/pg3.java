fefdf19bbb671de72cd52f8a329672b4cb329e92
valeng1133 <est.yulianav.gomez@unimilitar.edu.co>
2024-08-27 15:43:26 -0500

    Create pg3.java

---
+++ P1/pg3.java
@@ -0,0 +1,15 @@
+//Crear dos métodos puros que permitan tener área y perimetro del triangulo asumiendoque todos son triangulos rectangulos 
+public class Triangulo{
+    private int base;
+    private int altura;
+public Triangulo (int b,int h);
+this.base = 2;
+this.altura = 5;
+return a+b +(Math.raiz(pow a,2)+(pow a,2);
+}
+Public main (String [] args){
+Scaner= new scanner; 
+System.println.out("Área del Triangulo= " +a *+b/2);
+return a*b/2;
+System.println.out("Perimetro del Triangulo= " +a +b+ Math.raiz(pow a,2)+(pow a,2);
+}
